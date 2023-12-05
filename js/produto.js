232e8d91931e9aada2d09ee6b34ab4573b2353c0
lU4N4ht <luana.maga78@gmail.com>
2023-12-05 14:20:42 -0300

    página de produto iniciada

---
+++ js/produto.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.querySelector(".heart-like-button");
+  
+    button.addEventListener("click", () => {
+      if (button.classList.contains("liked")) {
+        button.classList.remove("liked");
+      } else {
+        button.classList.add("liked");
+      }
+    });
+  });
